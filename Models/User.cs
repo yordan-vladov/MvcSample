36e39c5a4e238d3293760979fbdca548e468f09a
Yordan Vladov <jordan0vladov@gmail.com>
2024-03-09 14:02:31 +0200

    Added user model

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Username { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+    public string? Password;
+}
